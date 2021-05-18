e71655ee0f732491660a9d8dad635de8cda6ada0
jesusmataroman <84407967+jesusmataroman@users.noreply.github.com>
2021-05-18 22:29:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaciondeConsola5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int num1 = 5;
+            int num2 = 6;
+            double num3 = 2.57;
+            double suma;
+            suma = num1 + (-num2) + num3;
+
+            Console.WriteLine(suma);
+            String val1 = Console.ReadLine();
+            Console.WriteLine("El valor introducido es:" + val1);
+            int valint = Convert.ToInt32(val1);
+
+            Console.ReadKey();
+        }
+    }
+}
